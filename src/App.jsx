4ac46f2839a6da27c5d7aa5056fa2584416860c0
RupinVijan <rupinvijan@gmail.com>
2022-09-05 00:46:47 +0530

    setup commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './pages/Home'
+import CollectionDescription  from './pages/CollectionDescription'
+import Collections  from './pages/Collections'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <div>
+       <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/CollectionDescription" element={<CollectionDescription />} />
+          <Route path="/Collections" element={<Collections />} />
+
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
