0794a999018a693133844c03e66292056cf4fcff
LordFLader <devyansh1kalra@gmail.com>
2022-09-06 01:41:55 +0530

    added cards to decription page

---
+++ src/components/Card.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import '../assets/css/card.css';
+import NFT1 from "../assets/images/NFT_img.png";
+import NFT2Back from "../assets/images/image_14.png";
+
+const Card = () => {
+    return (
+        
+        <div class="card" style={{
+        width: "12rem", 
+        background: "grey", 
+        display: "flex",
+        alignItems:"center",
+        borderRadius:"5%",
+        color: "white",
+        marginRight:"2%",
+        marginBottom:"2%",
+        fontSize:"14px"
+        }}>
+            <img class="card-img-top" src={NFT1} alt="Card image cap" style={{borderRadius:"5%", backgroundImage:`url(${NFT2Back})`, backgroundSize:"cover", width:"90%", 
+            marginTop:"5%"
+            }} />
+            <div class="card-body" style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "100%",
+                paddingBottom:"0",
+                paddingRight:"5px"
+            }}>
+                <div class="card-text" >
+                    <h5>
+                    XXXX
+                    </h5>
+                </div>
+                <div class="card-text">
+                   <button type="button"  style = {{borderRadius:"25%", background:"lightgreen",border:"none",color:"white"}}>BUY NOW</button>
+                </div>
+                
+            </div>
+
+            <div class="card-body" style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "100%",
+                paddingBottom:"0",
+                paddingTop:"0"
+            }}>
+                <div class="card-text">
+                    XXXX
+                </div>
+                <div class="card-text">
+                    XXXX
+                </div>
+            </div>
+
+            <div class="card-body" style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "100%",
+                paddingTop:"0"
+            }}>
+                <div class="card-text">
+                    Floor Price
+                </div>
+                <div class="card-text">
+                    XXXX
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- src/pages/CollectionDescription.jsx
+++ src/pages/CollectionDescription.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import mainImage from "../assets/images/NFT_img.png";
-
+import Card from "../components/Card";
 import "../assets/css/collectiondescription.css";
 const CollectionDescription = () => {
 
@@ -34,10 +34,10 @@ const CollectionDescription = () => {
   }
 
   return (
-    <div className="collection d-flex col justify-content-center  ">
+    <div className="collection d-flex col justify-content-center ">
       <div className="collection__container d-flex flex-wrap align-items-start  ">
-        <div className="collection__image d-flex justify-content-center m-5 ">
-          <img src={mainImage} alt="fireSpot" />
+        <div className="collection__image  d-flex justify-content-center m-5 ">
+          <img src={mainImage}  alt="fireSpot" />
         </div>
         <div className="collection__info m-5">
           <div className="info__gdsc d-flex flex-wrap justify-content-between ">
@@ -49,12 +49,12 @@ const CollectionDescription = () => {
                 <div className="col col1 px-3 p-1 "><i class="bi bi-arrow-clockwise" onClick={handleRefresh}></i></div>
                 <div className="col col2 px-3 p-1"><a href="" target="_blank">
                   <svg xmlns="http://www.w3.org/2000/svg" width="28" height="31" fill="currentColor" class="bi bi-indent align-items-center d-flex" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M3 8a.5.5 0 0 1 .5-.5h6.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H3.5A.5.5 0 0 1 3 8Z"/>
-  <path fill-rule="evenodd" d="M12.5 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5Z"/>
-</svg></a></div>
+               <path fill-rule="evenodd" d="M3 8a.5.5 0 0 1 .5-.5h6.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H3.5A.5.5 0 0 1 3 8Z"/>
+                <path fill-rule="evenodd" d="M12.5 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5Z"/>
+                </svg></a></div>
                 <div className="col col3 px-3 p-1  "><svg onClick={handleShare} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share-fill" viewBox="0 0 16 16">
-  <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z"/>
-</svg></div>
+                   <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z"/>
+                 </svg></div>
               </div>
             </div>
           </div>
@@ -100,14 +100,40 @@ const CollectionDescription = () => {
                   <button class="btn-hover color-1">Buy Now</button>
                   <button class="color-2">Make Offer</button>
                 </div>
-              </div>
-            
+              </div> 
           </div>
-          
-          <div></div>
         </div>
         
+        
       </div>
+      <div className="info__priceBox increaseWidth m-5">
+            
+              <div className="col priceCol px-4  d-flex justify-content-start ">
+                <span className="d-flex align-items-center  py-2  ">
+                <i class="bi bi-clock px-3">
+                  
+                </i>
+                 View more from this collection</span>
+              </div>
+
+
+              <div className="col currentPriceCol   px-4 py-2">
+                
+                <div className=" cardsPrice d-flex px-4 justify-content-around ">
+                
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                
+                </div>
+                <div class="buttons">
+                  
+                  <button class="color-2">View Collections</button>
+                </div>
+              </div> 
+          </div>
+
       
     </div>
   );
