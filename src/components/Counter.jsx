54fa774e923ae9e8465a1c3f6b12871c31b290f2
Unnat Das <84987518+Sayo1305@users.noreply.github.com>
2022-09-05 22:19:27 +0530

    Update Counter.jsx

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,16 +5,18 @@ import React from "react";
 export default function Counter() {
   return (
     <div>
-      <div className="grid-container">
-        <div className="grid1">
-        <div className="grid-item1">Collections</div>
-        <div className="grid-item2">Artist</div>
-        <div className="grid-item3">Community</div>
+      <div className="CounterHeroContainer">
+        <div className="CounterHeroTextContainer">
+          <div className = "CounterHeroText1">Collections</div>
+          <div className="CounterHeroText2"><CountUp end={450} duration={2} />K+</div>
         </div>
-        <div className="grid2">
-        <div className="grid-item4"><CountUp end={450} duration={2} />K+</div>
-        <div className="grid-item5"><CountUp end={320} duration={2} />K+</div>
-        <div className="grid-item6"><CountUp end={16} duration={2} />K+</div>
+        <div className="CounterHeroTextContainer">
+          <div className = "CounterHeroText1">Artist</div>
+          <div className="CounterHeroText2"><CountUp end={320} duration={2} />K+</div>
+        </div>
+        <div className="CounterHeroTextContainer">
+          <div className = "CounterHeroText1">Community</div>
+          <div className="CounterHeroText2"><CountUp end={16} duration={2} />K+</div>
         </div>
       </div>
    
